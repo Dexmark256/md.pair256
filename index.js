a8dafe93757cac6b46cb935298b213bc502f8e50
Dexmark Tech <dexmarkdexmark@gmail.com>
2026-01-05 23:29:01 +0300

    Refactor index.js for improved structure and clarity

--- index.js
+++ index.js
@@ -1,33 +1,43 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const path = require('path');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Increase listener limit (Baileys safe)
+require('events').EventEmitter.defaultMaxListeners = 500;
+
+// Middleware MUST be before routes
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-__path = process.cwd()
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
-require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/server', server);
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/qr',async (req, res, next) => {
-res.sendFile(__path + '/qr.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+const __path = process.cwd();
+
+// Routes
+const server = require('./qr');
+const code = require('./pair');
+
+app.use('/server', server);
+app.use('/code', code);
+
+app.get('/pair', (req, res) => {
+  res.sendFile(path.join(__path, 'pair.html'));
+});
+
+app.get('/qr', (req, res) => {
+  res.sendFile(path.join(__path, 'qr.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__path, 'main.html'));
+});
+
+// Start server (Render-safe)
 app.listen(PORT, () => {
-    console.log(`
-Don't Forgot To Give Star MALVIN-XD
+  console.log(`Server running on port ${PORT}`);
+});
 
- Server running on http://localhost:` + PORT)
-})
-
-module.exports = app
+module.exports = app;
