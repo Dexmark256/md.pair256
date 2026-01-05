5581a9f7c33cec73210b1e3b3553a93616b89e6b
Dexmark Tech <dexmarkdexmark@gmail.com>
2026-01-05 20:41:21 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -13,7 +13,7 @@ function removeFile(FilePath) {
 router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
-    async function MALVIN_XD_PAIR_CODE() {
+    async function MAJIN_BUU_PAIR_CODE() {
         const {
             state,
             saveCreds
@@ -75,7 +75,7 @@ var randomItem = selectRandomItem(items);
                         const { upload } = require('./mega');
                         const mega_url = await upload(fs.createReadStream(rf), `${sock.user.id}.json`);
                         const string_session = mega_url.replace('https://mega.nz/file/', '');
-                        let md = "malvin~" + string_session;
+                        let md = "dante~" + string_session;
                         let code = await sock.sendMessage(sock.user.id, { text: md });
                         let desc = `*Hey there, MAJIN-BUU User!* 👋🏻
 
